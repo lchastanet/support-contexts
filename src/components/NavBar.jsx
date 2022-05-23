e1117bee043ffd95fa953185689ccca0fcbeb717
Loris <loris.ec@outlook.com>
2022-05-23 16:04:38 +0200

    filter list

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,23 @@
+import { useContext } from "react"
+
+import CurrentHousesContext from "../contexts/housesContext"
+
+import housesToRentData from "../data/housesToRent"
+
 function NavBar() {
+  const { setHousesToRent } = useContext(CurrentHousesContext)
+
+  const handleChange = (e) => {
+    const newList = housesToRentData.filter(
+      (house) => house.type === e.target.value || e.target.value === "All"
+    )
+    setHousesToRent(newList)
+  }
+
   return (
     <nav>
       <h1>NavBar</h1>
-      <select name="home">
+      <select name="home" onChange={handleChange}>
         <option value="All">All</option>
         <option value="Flat">Flat</option>
         <option value="House">House</option>
--- src/contexts/housesContext.jsx
+++ src/contexts/housesContext.jsx
@@ -1,12 +1,14 @@
-import { createContext } from "react"
+import { createContext, useState } from "react"
 
-import housesToRent from "../data/housesToRent"
+import housesToRentData from "../data/housesToRent"
 
 const CurrentHousesContext = createContext()
 
 export const CurrentHousesContextProvider = ({ children }) => {
+  const [housesToRent, setHousesToRent] = useState(housesToRentData)
+
   return (
-    <CurrentHousesContext.Provider value={{ housesToRent }}>
+    <CurrentHousesContext.Provider value={{ housesToRent, setHousesToRent }}>
       {children}
     </CurrentHousesContext.Provider>
   )
